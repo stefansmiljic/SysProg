ab97912d3fd068910364a34687aa9e3a00c3e99f
unknown <stefan.smiljic@elfak.rs>
2023-05-09 17:07:12 +0200

    dodavanje projekta 1

---
+++ ExcelWebServer/Program.cs
@@ -0,0 +1,8 @@
+﻿using ExcelWebServer;
+
+HttpServer server = new HttpServer();
+server.Start();
+Console.WriteLine("Press any key to stop the server...");
+Console.ReadKey();
+server.Stop();
+Console.WriteLine("Server stopped!");
